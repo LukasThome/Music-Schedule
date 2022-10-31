8a24cf4a7c28e63f4929886af1d5d9368bb7c0d2
LukasThome <thome_lucas@live.com>
2022-10-31 14:32:49 -0300

    alpha 0.0.2

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from controle.controlador_sistema import ControladorSistema
+print("\n")
+print("Alpha v0.0.2 10/31/22022 Segunda-Feira")
 
 if __name__ == "__main__":
   ControladorSistema().inicializa_sistema()
   
-print("Alpha v0.0.1 10/31/22022 Segunda-Feira")
+
